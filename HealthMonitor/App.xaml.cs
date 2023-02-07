ee9d135ce54b55cb9078442e5b5aa5a6b79edaff
jiangr <jiangrui@rhtect.com>
2023-02-07 10:22:43 +0800

    程序单例启动

--- HealthMonitor/App.xaml.cs
+++ HealthMonitor/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows;
 
 namespace HealthMonitor
 {
@@ -7,6 +11,34 @@ namespace HealthMonitor
     /// </summary>
     public partial class App : Application
     {
+        private const string MUTEX_NAME = "Global\\HealthMonitor";
+        private static Mutex AppMutex;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            /*创建具有唯一名称的互斥锁*/
+            AppMutex = new Mutex(true, MUTEX_NAME, out var createdNew);
 
+            if (!createdNew)
+            {
+                var current = Process.GetCurrentProcess();
+
+                foreach (var process in Process.GetProcessesByName(current.ProcessName))
+                {
+                    if (process.Id != current.Id)
+                    {
+                        SetForegroundWindow(process.MainWindowHandle);
+                        break;
+                    }
+                }
+                Shutdown();
+            }
+            else
+            {
+                base.OnStartup(e);
+            }
+        }
+
+        [DllImport("user32.dll", EntryPoint = "SetForegroundWindow", SetLastError = true)]
+        public static extern void SetForegroundWindow(IntPtr hwnd);
     }
 }
